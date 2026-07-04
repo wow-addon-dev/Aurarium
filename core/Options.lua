917beedea16336c97a63b6b13de2fe15d9363e60
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-07-04 12:22:52 +0200

    v2.15

--- core/Options.lua
+++ core/Options.lua
@@ -65,54 +65,54 @@ function Options:Initialize()
 
 	-- Minimap Button
 	AWL.Settings:AddCheckbox(category, {
-		variableTable = minimapButtonProxy,
-		settingKey    = addonName .. "_hide",
-		variableName  = "hide",
-		name          = L["options.general.minimap-button.name"],
-		tooltip       = L["options.general.minimap-button.tooltip"],
-		default       = true
+		variableTable	= minimapButtonProxy,
+		settingKey		= addonName .. "_hide",
+		variableName	= "hide",
+		name			= L["options.general.minimap-button.name"],
+		tooltip			= L["options.general.minimap-button.tooltip"],
+		default			= true
 	})
 
 	-- Debug Mode
 	AWL.Settings:AddCheckbox(category, {
-		variableTable = AUR.Settings.general,
-		settingKey    = addonName .. "_debug-mode",
-		variableName  = "debug-mode",
-		name          = L["options.general.debug-mode.name"],
-		tooltip       = L["options.general.debug-mode.tooltip"],
-		default       = false
+		variableTable	= AUR.Settings.general,
+		settingKey		= addonName .. "_debug-mode",
+		variableName	= "debug-mode",
+		name			= L["options.general.debug-mode.name"],
+		tooltip			= L["options.general.debug-mode.tooltip"],
+		default			= false
 	})
 
 	layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(L["options.currency-overview"]))
 
 	-- Open on Login
 	AWL.Settings:AddCheckbox(category, {
-		variableTable = AUR.Settings.currencyOverview,
-		settingKey    = addonName .. "_open-on-login",
-		variableName  = "open-on-login",
-		name          = L["options.currency-overview.open-on-login.name"],
-		tooltip       = L["options.currency-overview.open-on-login.tooltip"],
-		default       = false
+		variableTable	= AUR.Settings.currencyOverview,
+		settingKey		= addonName .. "_open-on-login",
+		variableName	= "open-on-login",
+		name			= L["options.currency-overview.open-on-login.name"],
+		tooltip			= L["options.currency-overview.open-on-login.tooltip"],
+		default			= false
 	})
 
 	-- Hide Unchanged Entries
 	AWL.Settings:AddCheckbox(category, {
-		variableTable = currencyOverviewProxy,
-		settingKey    = addonName .. "_hide-unchanged-entries",
-		variableName  = "hide-unchanged-entries",
-		name          = L["options.currency-overview.hide-unchanged-entries.name"],
-		tooltip       = L["options.currency-overview.hide-unchanged-entries.tooltip"],
-		default       = false
+		variableTable	= currencyOverviewProxy,
+		settingKey		= addonName .. "_hide-unchanged-entries",
+		variableName	= "hide-unchanged-entries",
+		name			= L["options.currency-overview.hide-unchanged-entries.name"],
+		tooltip			= L["options.currency-overview.hide-unchanged-entries.tooltip"],
+		default			= false
 	})
 
 	-- Profiles Section
 	AWL.Settings:AddProfilesSection(layout, {
-		useAccountProfile = Utils:IsAccountProfile(),
-		onSwitchProfile = function()
+		useAccountProfile			= Utils:IsAccountProfile(),
+		onSwitchProfile				= function()
 			Utils:ToggleProfileMode()
 			ReloadUI()
 		end,
-		onDeleteCharacterProfiles = function()
+		onDeleteCharacterProfiles	= function()
 			Utils:ResetAllCharacterProfiles()
 			ReloadUI()
 		end
